0002622d24ddd71ba8985e09b532b44b64777edf
CyberL1 <mateusz.hf18@gmail.com>
2024-12-03 14:59:19 +0100

    feat: cat command

---
+++ src/shell/commands/cat.ts
@@ -0,0 +1,26 @@
+import type { Command } from "../../types.ts";
+import { existsSync, readFileSync, statSync } from "fs";
+
+const command: Command = {
+  name: "cat",
+  run: async (interaction, [path]) => {
+    const { env } = interaction.client.shell.users.get(interaction.user.id);
+
+    const realPath = interaction.client.shell.fs.from(
+      interaction.client.shell.fs.translate(env, path),
+    );
+
+    if (!existsSync(realPath)) {
+      return { code: 1, message: "Path does not exist" };
+    }
+
+    if (statSync(realPath).isDirectory()) {
+      return { code: 1, message: "Path is a directory" };
+    }
+
+    const fileContents = readFileSync(realPath, { encoding: "utf-8" });
+    return { code: 0, message: fileContents };
+  },
+};
+
+export default command;

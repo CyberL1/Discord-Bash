0118309456c857aec4d95562726f3080dc735d60
CyberL1 <mateusz.hf18@gmail.com>
2025-01-04 13:11:27 +0100

    chore: remove ununsed type

--- src/shell/types.ts
+++ src/shell/types.ts
@@ -5,7 +5,6 @@ export interface Command {
   description: string;
   args?: Arguments;
   flags?: Flags;
-  superuserOnly?: boolean;
   run: (
     interaction: ChatInputCommandInteraction,
     args: ArgumentsParsed,
